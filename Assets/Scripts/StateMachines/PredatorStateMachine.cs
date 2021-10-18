b698535838c48cdcec3aca861c60d42abea2ab29
JTawdawd <80257428+JTawdawd@users.noreply.github.com>
2021-10-18 03:16:06 +0100

    Added State Machines
    The outline for the prey state machine and the predator state machine. The initial behaviours will be made in the state machines.

---
+++ Assets/Scripts/StateMachines/PredatorStateMachine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredatorStateMachine : StateMachine
+{
+    void Start()
+    {
+        SetState(new Idle(this));
+    }
+
+    // Predator needs to idle
+    // SetState(new Idle(this));
+
+    // Predator needs to hunt
+    // SetState(new Hunt(this));
+
+    // Predator needs to drink
+    // SetState(new Drink(this));
+
+    // Predator needs to reproduce
+    // SetState(new Reproduce(this));
+}
---
+++ Assets/Scripts/StateMachines/States/State.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State 
+{
+    protected StateMachine _stateMachine;
+
+    public State(StateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
+    public virtual IEnumerator OnStart()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Execution()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator OnExit()
+    {
+        yield break;
+    }
+}
