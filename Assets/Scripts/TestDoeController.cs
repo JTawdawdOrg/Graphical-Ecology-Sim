74491fc0f38809c5e7c9221fd53d09afdbedb261
JTawdawd <80257428+JTawdawd@users.noreply.github.com>
2021-10-22 05:33:44 +0100

    Added New Terrain
    New terrain using Unity's Navigator (nav mesh + nav agent) for movement. This commit is mainly just models, prefabs, and textures.

---
+++ Assets/Scripts/TestDoeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestDoeController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+
+            if (hit.transform.tag == "Ground")
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+                
+    }
+}
